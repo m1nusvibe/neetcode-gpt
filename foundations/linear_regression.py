8cefb4ebbe67689398fac9e940c99bdfcb79ae6f
m1nusvibe <vooper333@gmail.com>
2026-08-03 15:35:03 +0300

    Implement Linear regression forward pass (Math Foundations 5/6)

---
+++ foundations/linear_regression.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy.typing import NDArray
+
+class Solution:
+
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        y = np.dot(X, weights)
+        return np.round(y, 5)
+
+    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
+        mse = np.mean((model_prediction - ground_truth)**2)
+        return np.round(mse, 5)
